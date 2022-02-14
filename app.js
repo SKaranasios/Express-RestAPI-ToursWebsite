c0798647f5bd7d2f1f313f15cf25922524dc9a73
Skaranasios <stathis.karanasios@outlook.com>
2022-02-14 23:05:21 +0200

    url route refactoring, patch-delete requests added

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
-//app.use(express.json());
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-//its good practice for defining version of API
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   //statuscode,header
   res.status(200).json({
     status: 'success',
@@ -18,17 +17,33 @@ app.get('/api/v1/tours', (req, res) => {
       tours: tours,
     },
   });
-});
+};
 
-app.get('/api/v1/tours', (req, res) => {
+const getTour = (req, res) => {
   console.log(req.params);
+
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  // if (id > tours.length)
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  console.log(tour);
 
   res.status(200).json({
     status: 'success',
+    data: {
+      tour,
+    },
   });
-});
+};
 
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
@@ -46,7 +61,50 @@ app.post('/api/v1/tours', (req, res) => {
     }
   );
   //res.send('Done');
-});
+};
+const updateTour = (req, res) => {
+  //check if data exist
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid Id',
+    });
+  }
+
+  res.status(200).json({
+    status: 'sucess',
+    data: {
+      tour: '<updated tour here >',
+    },
+  });
+};
+
+const deleteTour = (req, res) => {
+  //check if data exist
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid Id',
+    });
+  }
+
+  //204 data doesnt exist
+  res.status(204).json({
+    status: 'sucess',
+    data: null,
+  });
+};
+
+//its good practice for defining version of API
+app.get('/api/v1/tours', getAllTours);
+
+app.get('/api/v1/tours/:id', getTour);
+
+app.post('/api/v1/tours', createTour);
+
+app.patch('/api/v1/tours/:id', updateTour);
+
+app.delete('/api/v1/tours/:id', deleteTour);
 
 const port = 3000;
 app.listen(port, () => {
