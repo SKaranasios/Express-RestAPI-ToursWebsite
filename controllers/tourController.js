f59e5d825a1d4041653cea23c4919f532ce1d22c
Skaranasios <stathis.karanasios@outlook.com>
2022-02-20 17:47:03 +0200

    filter,sort,select,pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -12,8 +12,61 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //1A)FILTERING
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
 
+    //1B)ADVANCED FILTERING
+    //will return a query object that why we can chain methods
+    let queryStr = JSON.stringify(queryObj);
+    //regex \b: exact same value  /g: multiple tiems
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    let query = Tour.find(JSON.parse(queryStr));
+    // console.log(JSON.parse(queryStr));
+    // console.log(queryStr);
+
+    //2)SORTING
+    //if it contains sort in it
+    if (req.query.sort) {
+      //sort query bases on attribute of sort
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+      //sort('price ratingsAverage')
+    }
+    //adding default query
+    else {
+      query = query.sort('-createdAt');
+    }
+    // console.log(req.query.sort);
+
+    //3)FILTERING
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+      //$select:('name duration')
+    } else {
+      query = query.select('-__v');
+      //here on the contraty of sorting with - we exclude the field.
+    }
+
+    //4)PAGINATION
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = limit * (page - 1);
+    //page=1&limit=10 , 1-10: page 1 ,11-20:page 2
+    query = query.skip(skip).limit(limit);
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      //throwing error will send us to catch method
+      if (skip >= numTours) throw new Error("this page doesn't exist");
+    }
+
+    //executing the query
+    const tours = await query;
+    //query.sort().select().skip().limit()
+
+    //3)RESPONSE
     res.status(200).json({
       status: 'success',
       requestedAt: req.requestTime,
@@ -28,6 +81,20 @@ exports.getAllTours = async (req, res) => {
       message: err
     });
   }
+  /*
+    const tours = await Tour.find({
+      duration: 5,
+      difficulty: 'easy'
+    });
+
+
+    const tours = await Tour.find()
+      .where('duration')
+      .equals(5)
+      .where('difficulty')
+      .equals('easy');
+
+     */
 };
 
 exports.getTour = async (req, res) => {
